15f5d47e7e3811c3149f4fe0280a90626323af00
Jonas Dieroff <152855101+QuantumVortex1@users.noreply.github.com>
2025-10-28 17:00:45 +0100

    FIXING

--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -76,13 +76,6 @@ resource "aws_security_group" "ec2_sg" {
   description = "Security group for ${var.project_name} EC2 instance"
   vpc_id      = var.vpc_id
   ingress {
-    description = "HTTP from load balancer only"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = var.allowed_http_cidrs
-  }
-  ingress {
     description = "HTTPS from load balancer only"
     from_port   = 443
     to_port     = 443
